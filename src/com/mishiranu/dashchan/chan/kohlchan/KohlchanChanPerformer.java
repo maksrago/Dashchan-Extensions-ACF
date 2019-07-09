7f864932b68947d6a6e5b732b73c82c59db81278
kellerbernd <52660155+kellerbernd@users.noreply.github.com>
2019-07-10 00:12:27 +0200

    Performer: Style, log unrecognised status replies, detect required captchas

--- src/com/mishiranu/dashchan/chan/kohlchan/KohlchanChanPerformer.java
+++ src/com/mishiranu/dashchan/chan/kohlchan/KohlchanChanPerformer.java
@@ -173,14 +173,17 @@ public class KohlchanChanPerformer extends ChanPerformer
 
 		String status = jsonObject.optString("status");
 		String response = jsonObject.optString("data");
-		if ("ok".equals(status))
-		{
+		if ("ok".equals(status)) {
 			String postNumber = response;
-			if(postNumber.isEmpty()) throw new InvalidResponseException();
+			if (postNumber.isEmpty()) throw new InvalidResponseException();
 			String threadNumber = data.threadNumber != null ? data.threadNumber : postNumber;
 			return new SendPostResult(threadNumber, postNumber);
-		} else if(!"error".equals(status))
+		} else if("bypassable".equals(status)) {
+			throw new ApiException(ApiException.SEND_ERROR_CAPTCHA);
+		} else if(!"error".equals(status)) {
+			CommonUtils.writeLog("Unrecognised post reply status: ", status);
 			throw new InvalidResponseException();
+		}
 
 		int errorType = 0;
 		if (response.contains("Either a message or a file is required"))
@@ -232,16 +235,17 @@ public class KohlchanChanPerformer extends ChanPerformer
 				.setRedirectHandler(HttpRequest.RedirectHandler.STRICT).read().getJsonObject();
 
 		String status = jsonObject.optString("status");
-		if ("ok".equals(status))
-		{
+		if ("ok".equals(status)) {
 			JSONObject response = jsonObject.optJSONObject("data");
 			if (response != null && response.optInt("removedThreads") == 0
 					&& response.optInt("removedPosts") == 0)
 				throw new ApiException(ApiException.DELETE_ERROR_PASSWORD); // can't tell why it failed
 
 			return null;
-		} else if(!"error".equals(status))
+		} else if(!"error".equals(status)) {
+			CommonUtils.writeLog("Unrecognised post deletion status: ", status);
 			throw new InvalidResponseException();
+		}
 
 		String response = jsonObject.optString("data");
 		CommonUtils.writeLog("Delete post failed: ", response);
